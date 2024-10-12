0c9c3a53f111286712fdc3cec8de0e1f8144cf25
won <andrew112517@naver.com>
2024-10-12 16:32:35 +0900

    [Add] UIView Extension추가

---
+++ Week2/Week2/Resources/Extensions/UIView+.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+.swift
+//  Week2
+//
+//  Created by 김승원 on 10/12/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // UIView 여러 개 인자로 받아서 한 번에 addSubview하는 메소드
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+}
