0596ed1460380d6e3ebe1b29b827a2c6f18cbd8a
won <andrew112517@naver.com>
2024-10-12 16:27:42 +0900

    []

---
+++ Week2/Week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by 김승원 on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Week2/Week2UITests/Week2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Week2UITestsLaunchTests.swift
+//  Week2UITests
+//
+//  Created by 김승원 on 10/12/24.
+//
+
+import XCTest
+
+final class Week2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
