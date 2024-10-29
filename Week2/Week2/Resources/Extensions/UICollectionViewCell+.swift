752e9758ca0c2f3f2978097ac1b41ee7e0e3ea43
won <andrew112517@naver.com>
2024-10-30 00:53:42 +0900

    [add] UICollectionViewCell extension

---
+++ Week2/Week2/Resources/Extensions/UICollectionViewCell+.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewCell+.swift
+//  Week2
+//
+//  Created by 김승원 on 10/30/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    // Cell identifier
+    static var cellIdentifier: String {
+        return String(describing: self)
+    }
+}
