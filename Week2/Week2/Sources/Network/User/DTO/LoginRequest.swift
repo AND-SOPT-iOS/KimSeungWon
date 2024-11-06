947075176714c51ebd35963f12c3cda8e0130dac
won <andrew112517@naver.com>
2024-11-06 20:27:20 +0900

    [add] Dto 생성

---
+++ Week2/Week2/Sources/Network/User/DTO/LoginRequest.swift
@@ -0,0 +1,13 @@
+//
+//  LoginRequest.swift
+//  Week2
+//
+//  Created by 김승원 on 11/6/24.
+//
+
+import Foundation
+
+struct LoginRequest: Codable {
+    let username: String
+    let password: String
+}
--- Week2/Week2/Sources/Network/User/DTO/RegisterRequest.swift
+++ Week2/Week2/Sources/Network/User/DTO/RegisterRequest.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct RegisterRequest: Codable {
-  let username: String
-  let password: String
-  let hobby: String
+    let username: String
+    let password: String
+    let hobby: String
 }
